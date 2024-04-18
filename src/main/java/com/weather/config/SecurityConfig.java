63597bb28589003edbeed529a6f3340b7a1bb52c
tkavya <tkavya@tkavya-lp.pkrmsezdc.com>
2024-04-18 16:32:18 +0530

    Commit

--- src/main/java/com/weather/config/SecurityConfig.java
+++ src/main/java/com/weather/config/SecurityConfig.java
@@ -1,44 +1,45 @@
 package com.weather.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.web.filter.CorsFilter;
-
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	 
-	 @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-         http.csrf(csrf -> csrf.disable())
-                 .authorizeRequests(authorize -> authorize
-                         .antMatchers("/weather**").authenticated()
-                         .anyRequest().permitAll())
-                 .httpBasic(withDefaults());
-	    }
-
-	 
-	 @Override
-	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	        auth.inMemoryAuthentication()
-	            .withUser("user")
-	            .password(passwordEncoder().encode("password"))
-	            .roles("USER_ROLE");
-	    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf((csrf) -> csrf.disable())
+		.authorizeHttpRequests((authorizeHttpRequests) ->
+	 		authorizeHttpRequests
+	 	.requestMatchers("/weather**").authenticated()	
+		.anyRequest().permitAll())
+		.httpBasic(withDefaults())
+		 .formLogin(login->
+		  login
+		  .permitAll());
+              ;
+		return httpSecurity.build();
+	}
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.builder()
+				.username("user")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER_ROLE")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+	@Bean 
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
-
